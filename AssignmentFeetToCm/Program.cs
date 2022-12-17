9aa21642d82082750c25fe6c90fe48101dd35c2a
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-17 15:31:17 +0100

    Submitted Assignment: FeetToCm

--- AssignmentFeetToCm/Program.cs
+++ AssignmentFeetToCm/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿/*
+Write a C# program convert a person's height (in feet and inches) to centimeters.
+Eg:
+feet = 5
+inches = 7
+output: 170.18 cm
 
+Formula:
+1 inch = 2.54 cm
+1 feet = 12 inches
+*/
 namespace AssignmentFeetToCm
 {
 	class Program
 	{
 		static void Main()
 		{
-			Console.WriteLine("Hello World!");
+			// Inputs
+			int feet = 5, inches = 7;
+
+			// Process
+			// Find number of inches based on number of feet
+			int total_inches = (feet * 12) + inches;
+			// Find number of centimeters based on number of inches
+			double centimeters = total_inches * 2.54;
+
+			// Output:
+			System.Console.WriteLine("Person's height is " + centimeters);
+			System.Console.ReadKey();
 		}
 	}
 }
